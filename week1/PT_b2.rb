aec5ad2f7a964ca400e2583c619e74e869454c28
ThanhThao44 <thao441997@gmail.com>
2019-09-15 19:03:27 +0700

    Bai tap tuan 1

---
+++ week1/PT_b2.rb
@@ -0,0 +1,23 @@
+require 'cmath'
+a = 1
+b = -2
+c = 1
+
+a2 = a*2
+
+if a != 0 then
+  delta = (b*b) - (4*a*c)
+  sqrt_delta = CMath.sqrt(delta)
+  if delta > 0 then
+    x1 = (-b + sqrt_delta)/a2
+    x2 = (-b - sqrt_delta)/a2
+    puts "Phuong trinh co 2 nghiem: x1 = #{x1} ; x2 = #{x2}"
+  elsif delta == 0 then
+    x = -b/a2
+    puts "Phuong trinh co nghiem kep : x1 = x2 = #{x}"
+  else
+    puts "Phuong trinh vo nghiem"
+  end
+else
+  puts "Day khong phai phuong trinh bac 2"
+end
---
+++ week1/fizzbuzz.rb
@@ -0,0 +1,15 @@
+for i in 1..15
+	if i%15 == 0 then
+		puts "FIZZBUZZ"
+	elsif i%3 == 0 then
+		puts "FIZZ"
+	elsif i%5 == 0 then
+		puts "BUZZ"
+	else
+		puts "#{i}"
+	end
+end
+
+
+			
+			
